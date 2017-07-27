bd9dceadd2a9d04d80c5ffdb35aa4c80338b0e13
Diana Mkrtchyan <dianam@dianam.local>
2017-07-27 10:39:58 -0500

    create event-details component

---
+++ app/events/event-details/event-details.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core'
+// import {EventService} from './shared/event.service'
+// import {ToastrService} from '../common/toastr.service'
+
+@Component({
+	templateUrl: 'app/events/event-details/event-details.component.html',
+	styles: [`
+		.container: {padding-left: 20px; padding-right: 20px;}
+		.event-image: {height: 100px;}
+	`
+	]
+}) 
+
+export class EventDetailComponent {
+
+}
