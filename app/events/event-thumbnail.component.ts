199740031c23567aaa10c93fc8924b0bae6d2a7b
Diana Mkrtchyan <dianam@dianam.local>
2017-07-27 10:25:57 -0500

    Initial commit

---
+++ app/events/event-thumbnail.component.ts
@@ -0,0 +1,25 @@
+import {Component, Input} from '@angular/core'
+
+@Component({
+	selector: 'event-thumbnail',
+	template:`
+		<div class="well hoverwell thumbnail">
+			<h2>{{event?.name}}</h2>
+			<div>Date: {{event?.date}}</div>
+			<div [ngSwitch]="event?.time">
+				Time: {{event?.time}}
+				<span *ngSwitchCase="'8:00 am'">(Early start)</span>
+				<span *ngSwitchCase="'10:00 am'">(Late start)</span>
+				<span *ngSwitchDefault>(Normal start)</span>
+			</div>
+			<div>Price: \${{event.price}}</div>
+			<div *ngIf="event?.location">
+				<span>Location: {{event?.location?.address}}</span>
+			</div>
+		</div>
+	`
+})
+
+export class EventThumbnailComponent{
+	@Input() event:any
+}
